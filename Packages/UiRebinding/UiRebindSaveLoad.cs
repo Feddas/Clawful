3f38ea310bbf8e0acc8669c6c37d22b136aac957
Feddas <feddas00@gmail.com>
2024-09-08 11:38:31 -0700

    InputSystem rebinding sample as UPM package with changes from https://youtu.be/qXbjyzBlduY

---
+++ Packages/UiRebinding/UiRebindSaveLoad.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary> copied from https://www.youtube.com/watch?v=csqVa2Vimao&t=1970s </summary>
+public class UiRebindSaveLoad : MonoBehaviour
+{
+    /// <summary> PlayerPrefs key used to store rebinds </summary>
+    public static readonly string Key = "rebinds";
+
+    public InputActionAsset actions;
+
+    public void OnEnable()
+    {
+        Load();
+    }
+
+    public void Save()
+    {
+        var rebinds = actions.SaveBindingOverridesAsJson();
+        PlayerPrefs.SetString(Key, rebinds);
+    }
+
+    public void Load()
+    {
+        var rebinds = PlayerPrefs.GetString(Key);
+        if (false == string.IsNullOrEmpty(rebinds))
+        {
+            actions.LoadBindingOverridesFromJson(rebinds);
+        }
+    }
+
+    public void SaveThenDeactivate()
+    {
+        Save();
+        this.gameObject.SetActive(false);
+    }
+}
