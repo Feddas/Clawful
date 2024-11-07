2430a3f74134058fe4adb9344c2caf0e874145ae
Feddas <feddas00@gmail.com>
2024-11-06 17:57:42 -0800

    ShareDevice

---
+++ Packages/UiRebinding/ShareDevice/PlayerInputRespawn.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ShareDevice
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerInputRespawn : MonoBehaviour
+    {
+        /// <summary> Position where this player will be re/spawned. Move this transform when a checkpoint is reached. </summary>
+        public TransformAtIndex Respawn { get; private set; }
+
+        /// <summary> Required <see cref="PlayerInput"/> component invokes this function via UnityEvent when DropPlayer action is triggered. </summary>
+        public void OnDropPlayer(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Destroy(gameObject);
+                // TODO: remove PlayerInputManager dependancy on NotifyPlayerLeft. Instead create a C# event, OnDropped, for TestDropPlayer to subscribe to.
+            }
+        }
+
+        public virtual void SetPosition(TransformAtIndex spawn)
+        {
+            Respawn = spawn;
+            transform.position = Respawn.Transform.position;
+        }
+    }
+}
